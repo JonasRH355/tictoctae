308f38a8210426696fe4aabe4b3080626842d40a
Booxmann - Jonas <jonatas.hellmann355@gmail.com>
2024-04-22 17:29:43 -0300

    Input system
    I just import the .Scanner and test with a simple form

--- Main.java
+++ Main.java
@@ -1,18 +1,20 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Model jModel = new Model();
         View tabuleiro = new View();
+        Scanner obj = new Scanner(System.in);
+        while (true) {
+            
+            jModel.setInTabuleiro(0,2,1);
+            int aux = obj.nextInt();
+            jModel.setInTabuleiro(0,0,aux);
+            tabuleiro.showtable(jModel);
+            System.out.print(obj);
+            break;
+        }
 
-
-
-        jModel.setInTabuleiro(0,2,1);
-        jModel.setInTabuleiro(0,0,3);
-       
-        
-
-        tabuleiro.showtable(jModel);
     }
     
 }

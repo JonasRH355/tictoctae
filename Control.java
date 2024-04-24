7953b6e6dea433625d6bedd36c07d611cba818b1
Booxmann - Jonas <jonatas.hellmann355@gmail.com>
2024-04-24 20:31:30 -0300

    collision system
    Now, you can't set something in a place that already have something

--- Control.java
+++ Control.java
@@ -8,14 +8,14 @@ public class Control {
 
     public void reloadgame(){
         jModel.setInTabuleiro(0,2,3);
-        System.out.print("\033[H\033[2J");
-        System.out.flush();                   // It's a terminal' clear
 
         while (loop) {  
             System.out.print("\033[H\033[2J");
             System.out.flush();                   // It's a terminal' clear
+
             table.showtable(jModel); 
             inPutX();
+            botInput();
             jModel.reloadAddValTab();
             if (endGame()) {
                 loop=false;
@@ -26,9 +26,20 @@ public class Control {
 
     protected void inPutX(){
         System.out.print("Onde vai: (i,j)");
-        int i = obj.nextInt();
-        int j = obj.nextInt();
+        int i = 0;
+        int j = 0;
+        do{
+            i = obj.nextInt();
+            j = obj.nextInt();
+        }while(alreadyHave(i,j));
+        
         jModel.setInTabuleiro(i,j,1);
+    }
+
+    private boolean alreadyHave(int i, int j){
+        boolean x = (jModel.table[i][j] != 2)? true:false;
+        return x;
+        
     }
 
     protected boolean endGame() {
@@ -46,5 +57,9 @@ public class Control {
         } else {
             return false;
         }
+    }
+
+    protected boolean botInput(){
+        return true;
     }
 }
